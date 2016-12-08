cf3eaaba66f9acb12feca059a654db3da0cef3be
ZackExplosion <cstony0917@gmail.com>
2016-12-09 01:56:07 +0800

    setup docker

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20161122060228) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "trips", force: :cascade do |t|
     t.string   "video_link"
     t.string   "google_map_path_link"

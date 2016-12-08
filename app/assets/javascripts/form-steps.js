308341b3f7075947d5f04319e42be7881109059c
ZackExplosion <cstony0917@gmail.com>
2016-12-09 01:33:53 +0800

    add validations

--- app/assets/javascripts/form-steps.js
+++ app/assets/javascripts/form-steps.js
@@ -1,12 +1,63 @@
+var lating_validation = function(value){
+  console.log(value)
+  var is_valid = false
+
+  try {
+    value = JSON.parse(value)
+  } catch(e) {
+    // console.error(e)
+  }
+
+
+  if (typeof value.lat === 'number' && typeof value.lng === 'number') {
+    is_valid = true
+  }
+
+  return is_valid
+}
+
+var number_of_passenger_validation = function(value) {
+  value = parseInt(value)
+
+  if (typeof value !== 'number'){
+    return {
+      is_valid: false,
+      message: '乘客人數必須是一個數字'
+    }
+  }
+
+
+  if (value < 1 || value > 3) {
+    return {
+      is_valid: false,
+      message: '乘客人數不對，請不要四貼'
+    }
+  }
+
+  return true
+
+}
+
+var youtube_link_validation = function(input) {
+  return true
+}
+
 var form_steps = {
+  '#trip_video_link': {
+    notify_text: '驗證距離的影片',
+    validation: youtube_link_validation,
+  },
   '#trip_start_point': {
     notify_text: '點擊地圖選擇啟始點',
     click_on_map: true,
-    validation: '',
+    validation: lating_validation,
+    not_valid_message: '請點擊地圖選擇啟始點!'
   },
   '#trip_end_point': {
     notify_text: '點擊地圖選擇結束點',
     click_on_map: true,
+    validation: lating_validation,
+    not_valid_message: '請點擊地圖選擇結束點!'
   },
   '#trip_distance': {
     notify_text: '未來會有自動計算功能',
@@ -16,9 +67,10 @@ var form_steps = {
   },
   '#trip_numbers_of_people': {
     notify_text: '包含自己的乘客數量，建議不要三貼',
+    validation: number_of_passenger_validation
   },
-  '#trip_video_link': {
-    notify_text: '驗證距離的影片',
+  '#trip_accident': {
+    notify_text: '您是否有發生事故呢？',
   }
 }
 
@@ -57,9 +109,37 @@ function initFormSteps(map, form_steps) {
 
   var submit_form = false
   action_button.on('click submit', function(event){
+    var form_validation
 
-    current_step++
+    if( typeof current_action.validation !== 'function') {
+      form_validation = true
+    } else {
+      form_validation = current_action.validation(current_form_element.val())
+    }
 
+    var is_valid = form_validation
+    var not_valid_message = current_action.not_valid_message
+
+
+    // the return value maybe an object
+    if (typeof form_validation === 'object'){
+      is_valid = form_validation.is_valid
+      not_valid_message = form_validation.message
+    }
+
+    if ( is_valid ){
+      current_step++
+    } else {
+      console.log('input not valid')
+      current_form_element.notify(not_valid_message, {
+        className: 'error'
+      })
+      event.preventDefault()
+      return
+    }
+
+
+    // if last step, updateStepHint will return false
     if ( updateStepHint() ){
       current_marker = null
       event.preventDefault()
